f129fd4407dfdc002f4bb622cc62e6493089bbcd
Jessica <jwils117@emich.edu>
2017-12-06 15:44:04 -0500

    Alphabetize methods

--- customer.py
+++ customer.py
@@ -15,24 +15,28 @@ class Customer(object):
         for i in range(1, 4):
             mo_name = "mo" + str(i)
             self.moneyorders[mo_name] = self.create_moneyorder(mo_name)
+    
+    
+    def bit_commitment(self, id_int):
+        '''Bit commitment process
 
-    def create_moneyorder(self, name):
-        '''Creates an dict containing necessary money order fields'''
-        mo = {}
-        mo['name'] = name
-        mo['amount'] = self.amount
-        mo['uniqueness'] = self.random_num_generator()
-        mo['k'] = self.random_num_generator()
-        mo['I1'] = self.create_identity_string()
-        mo['I2'] = self.create_identity_string()
-        mo['I3'] = self.create_identity_string()
+        Using supplied split secret id_int use sha256 to create bit commitment
 
-        return mo
+        Return an array containing hash value and 2 randomly generated numbers
+        '''
+        # Get two random numbers
+        r1 = self.random_num_generator()
+        r2 = self.random_num_generator()
 
+        # Calculate the hash of the id int and the random numbers
+        # SHA256 library requires strings not integers, so ints are
+        # concatentated together and then hashed
+        int_string = str(id_int) + str(r1) + str(r2)
+        byte_string = bytes(int_string, encoding='utf-8')
+        hash_value = hashlib.sha256(byte_string).hexdigest()
+        int_value = int(hash_value, 16) % self.keys['n']
 
-    def print_moneyorder(self):
-        '''Method to print money order to file'''
-        pass
+        return [int_value, r1, r2]
 
 
     def blind(self):
@@ -76,6 +80,88 @@ class Customer(object):
             self.blind_moneyorders[mo] = blind_mo
 
 
+    def create_identity_string(self):
+        '''Creates the necessary pieces of the identity string
+
+        returns dict with:
+            id_string: [[left hash, left r1], [right hash, right r1]]
+            reveal_string: [[left generated nums], [right generated nums]]
+        '''
+        IL = {}
+        IR = {}
+
+        r, s = self.secret_splitting()
+        l_hash, r1, r2 = self.bit_commitment(id_int=r)
+        r_hash, s1, s2 = self.bit_commitment(id_int=s)
+        id_string = [[l_hash, r1], [r_hash, s1]]
+        reveal_array = [[r, r1, r2], [s, s1, s2]]
+
+        return {'id_string':id_string, 'reveal_array':reveal_array}
+
+
+    def create_moneyorder(self, name):
+        '''Creates an dict containing necessary money order fields'''
+        mo = {}
+        mo['name'] = name
+        mo['amount'] = self.amount
+        mo['uniqueness'] = self.random_num_generator()
+        mo['k'] = self.random_num_generator()
+        mo['I1'] = self.create_identity_string()
+        mo['I2'] = self.create_identity_string()
+        mo['I3'] = self.create_identity_string()
+
+        return mo
+
+
+    def print_moneyorder(self):
+        '''Method to print money order to file'''
+        pass
+
+
+    def secret_splitting(self):
+        '''Secret splitting process
+
+        Uses self.identity to create r and s.
+
+        Returns r and s
+        '''
+        r = self.random_num_generator()
+        s = r ^ self.identity
+
+        return r,s
+
+
+    def random_num_generator(self):
+        '''Generates a random number'''
+        # Parameters for random number generation
+        rand_low_num = 100
+        rand_high_num = 10000
+        return random.randint(rand_low_num, rand_high_num) % self.keys['n']
+
+
+    def reveal(self, moneyorders):
+        '''Reveals identity pieces for requested moneyorders
+        Takes in a list of money order keys (mo1, mo2, etc)
+
+        Returns self.moneyorders[mo*][r/s,r1,r2]
+        '''
+        revealed_nums = {}
+        for mo in moneyorders:
+            orig_mo = self.moneyorders[mo]
+            revealed_nums[mo] = {}
+            for key in orig_mo.keys():
+                if key.startswith('I'):
+                    revealed_nums[mo][key] = orig_mo[key]['reveal_array']
+
+        return revealed_nums
+
+
+    def receive_signature(self, moneyorder, signature):
+        '''Receive bank signature on money order and sign it'''
+        self.signed_moneyorder = self.blind_moneyorders[moneyorder]
+        self.signed_moneyorder['signature'] = signature
+
+
     def unblind(self, moneyorders):
         '''Unblinding process for money order
 
@@ -117,87 +203,3 @@ class Customer(object):
                                                          unblind_random])
 
             self.unblinded_moneyorders[mo] = unblind_mo
-
-
-    def secret_splitting(self):
-        '''Secret splitting process
-
-        Uses self.identity to create r and s.
-
-        Returns r and s
-        '''
-        r = self.random_num_generator()
-        s = r ^ self.identity
-
-        return r,s
-
-
-    def bit_commitment(self, id_int):
-        '''Bit commitment process
-
-        Using supplied split secret id_int use sha256 to create bit commitment
-
-        Return an array containing hash value and 2 randomly generated numbers
-        '''
-        # Get two random numbers
-        r1 = self.random_num_generator()
-        r2 = self.random_num_generator()
-
-        # Calculate the hash of the id int and the random numbers
-        # SHA256 library requires strings not integers, so ints are
-        # concatentated together and then hashed
-        int_string = str(id_int) + str(r1) + str(r2)
-        byte_string = bytes(int_string, encoding='utf-8')
-        hash_value = hashlib.sha256(byte_string).hexdigest()
-        int_value = int(hash_value, 16) % self.keys['n']
-
-        return [int_value, r1, r2]
-
-
-    def create_identity_string(self):
-        '''Creates the necessary pieces of the identity string
-
-        returns dict with:
-            id_string: [[left hash, left r1], [right hash, right r1]]
-            reveal_string: [[left generated nums], [right generated nums]]
-        '''
-        IL = {}
-        IR = {}
-
-        r, s = self.secret_splitting()
-        l_hash, r1, r2 = self.bit_commitment(id_int=r)
-        r_hash, s1, s2 = self.bit_commitment(id_int=s)
-        id_string = [[l_hash, r1], [r_hash, s1]]
-        reveal_array = [[r, r1, r2], [s, s1, s2]]
-
-        return {'id_string':id_string, 'reveal_array':reveal_array}
-
-
-    def random_num_generator(self):
-        '''Generates a random number'''
-        # Parameters for random number generation
-        rand_low_num = 100
-        rand_high_num = 10000
-        return random.randint(rand_low_num, rand_high_num) % self.keys['n']
-
-
-    def reveal(self, moneyorders):
-        '''Reveals identity pieces for requested moneyorders
-        Takes in a list of money order keys (mo1, mo2, etc)
-
-        Returns self.moneyorders[mo*][r/s,r1,r2]
-        '''
-        revealed_nums = {}
-        for mo in moneyorders:
-            orig_mo = self.moneyorders[mo]
-            revealed_nums[mo] = {}
-            for key in orig_mo.keys():
-                if key.startswith('I'):
-                    revealed_nums[mo][key] = orig_mo[key]['reveal_array']
-
-        return revealed_nums
-
-    def receive_signature(self, moneyorder, signature):
-        '''Receive bank signature on money order and sign it'''
-        self.signed_moneyorder = self.blind_moneyorders[moneyorder]
-        self.signed_moneyorder['signature'] = signature
