f8a692bc9aed42643bb42d84a034b902aecebf0a
Jessica <jwils117@emich.edu>
2017-12-06 15:39:31 -0500

    Transaction utilizing classes

---
+++ transaction.py
@@ -0,0 +1,19 @@
+import customer
+import bank
+
+alice_keys = {'e': 528, 'n': 4153}
+bob_keys = {'d': 936, 'n': 4153}
+amount = 100
+alice_id = 123432
+
+alice = customer.Customer(amount=amount, identity=identity, keys=alice_keys)
+bob = bank.Bank(keys=bob_keys)
+
+alice.blind()
+bob.receive_blindmoneyorders(alice.blind_moneyorders)
+bob.unblind_request()
+alice.unblind(bob.to_unblind_moneyorders)
+bob.receive_unblindedmoneyorders(alice.unblinded_moneyorders)
+bob.receive_revealinfo(alice.reveal(bob.to_unblind_moneyorders))
+bob.sign_moneyorder()
+alice.receive_signature(bob.to_sign_moneyorder_key, bob.bank_signature)
